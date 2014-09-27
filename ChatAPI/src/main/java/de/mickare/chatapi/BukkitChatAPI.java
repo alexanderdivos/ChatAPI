1dc1ed5704d3e74b1e6f26f509a6939001d33263
mickare <kaeser.micha@googlemail.com>
2014-09-27 02:05:28 +0200

    Zusätzlicher Chat Converter, der ProtocolLib nutzt.

--- ChatAPI/src/main/java/de/mickare/chatapi/BukkitChatAPI.java
+++ ChatAPI/src/main/java/de/mickare/chatapi/BukkitChatAPI.java
@@ -1,5 +1,6 @@
 package de.mickare.chatapi;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import de.mickare.chatapi.api.IChatBBCodeBuilder;
@@ -8,16 +9,22 @@ import de.mickare.chatapi.api.IChatBuilder;
 public class BukkitChatAPI {
 
 	private static enum Version {
-		v1_7_4
+		protocolLib, bukkit_v1_7_4
 	}
 
 	private static Version v = null;
 
 	private static Version getVersion() {
 		if (v == null) {
+			// ProtocolLib ftw!
+			if(Bukkit.getPluginManager().isPluginEnabled( "ProtocolLib" ))  {
+				v = Version.protocolLib;
+				return v;
+			}
+			// Standard CraftBukkit
 			try {
 				Class.forName( "net.minecraft.server.v1_7_R4.IChatBaseComponent", false, BukkitChatAPI.class.getClassLoader() );
-				v = Version.v1_7_4;
+				v = Version.bukkit_v1_7_4;
 			} catch (ClassNotFoundException e) {
 				throw new UnsupportedOperationException( e );
 			}
@@ -27,17 +34,19 @@ public class BukkitChatAPI {
 
 	public static IChatBuilder<Player> newBuilder() {
 		switch (getVersion()) {
-		case v1_7_4:
+		case bukkit_v1_7_4:
 			return new Chat_Bukkit_v1_7_4().newBuilder();
+		case protocolLib:
+			return new Chat_ProtocolLib().newBuilder();
 		default:
 			throw new UnsupportedOperationException();
 		}
 
 	}
-	
+
 	public static IChatBBCodeBuilder<Player> newBBCodeBuilder() {
 		switch (getVersion()) {
-		case v1_7_4:
+		case bukkit_v1_7_4:
 			return new Chat_Bukkit_v1_7_4().newBBCodeBuilder();
 		default:
 			throw new UnsupportedOperationException();
